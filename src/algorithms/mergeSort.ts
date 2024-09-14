fec86ae81bcef58a4491fb2aea5c77d2788cbf7a
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-14 09:52:13 +0300

    ⚡ General Update:  Making update in merge sort algorithm

--- src/algorithms/mergeSort.ts
+++ src/algorithms/mergeSort.ts
@@ -1,31 +1,69 @@
 import { AnimationArrayType } from "../types";
 
-function runInsertionSort(array: number[], animations: AnimationArrayType) {
-  for (let i = 1; i < array.length; i++) {
-    animations.push([[i], false]);
-    const currentValue = array[i];
-    let j = i - 1;
-    while (j >= 0 && array[j] > currentValue) {
-      animations.push([[j, j + 1, i], false]);
-      array[j + 1] = array[j];
-      animations.push([[j + 1, array[j]], true]);
-      j -= 1;
+function merge(
+  array: number[],
+  begin: number,
+  middle: number,
+  finish: number,
+  animations: AnimationArrayType
+) {
+  const left = array.slice(begin, middle);
+  const right = array.slice(middle, finish);
+
+  let i = 0;
+  let j = 0;
+  let k = begin;
+  while (i < left.length && j < right.length) {
+    animations.push([[begin + i, middle + j], false]);
+    if (left[i] <= right[j]) {
+      animations.push([[k, left[i]], true]);
+      array[k] = left[i];
+      i += 1;
+    } else {
+      animations.push([[k, right[j]], true]);
+      array[k] = right[j];
+      j += 1;
     }
-    array[j + 1] = currentValue;
-    animations.push([[j + 1, currentValue], true]);
+    k++;
+  }
+  while (i < left.length) {
+    animations.push([[begin + i], false]);
+    animations.push([[k, left[i]], true]);
+    array[k] = left[i];
+    i += 1;
+    k += 1;
+  }
+  while (j < right.length) {
+    animations.push([[middle + j], false]);
+    animations.push([[k, right[j]], true]);
+    array[k] = right[j];
+    j += 1;
+    k += 1;
   }
 }
 
-export function generateInsertionSortAnimationArray(
+function runMergeSort(array: number[]) {
+  const animations: AnimationArrayType = [];
+  for (let k = 1; k < array.length; k = 2 * k) {
+    for (let i = 0; i < array.length; i += 2 * k) {
+      const begin = i;
+      const middle = i + k;
+      const finish = Math.min(i + 2 * k, array.length);
+      merge(array, begin, middle, finish, animations);
+    }
+  }
+  return animations;
+}
+
+export function generateMergeSortAnimationArray(
   isSorting: boolean,
   array: number[],
   runAnimation: (animations: AnimationArrayType) => void
 ) {
   if (isSorting) return;
   if (array.length <= 1) return [];
-  const animations: AnimationArrayType = [];
 
   const auxiliaryArray = array.slice();
-  runInsertionSort(auxiliaryArray, animations);
+  const animations = runMergeSort(auxiliaryArray);
   runAnimation(animations);
 }
