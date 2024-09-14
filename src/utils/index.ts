0f915637645f69b70ae2d7bec09a369412fa6fd1
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-14 09:54:21 +0300

    ✨ New Feature: Building generateAnimationArray for each algorithm to simulate the animation

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,11 @@
+import { SortingAlgorithmType } from "../types";
+
+import { generateBubbleSortAnimationArray } from "../algorithms/bubbleSort";
+import { generateSelectionSortAnimationArray } from "../algorithms/selectionSort";
+import { generateInsertionSortAnimationArray } from "@/algorithms/insertionSort";
+import { generateMergeSortAnimationArray } from "../algorithms/mergeSort";
+import { generateQuickSortAnimationArray } from "../algorithms/quickSort";
+
 export function generateRandomNumberFromInterval(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -12,6 +20,33 @@ export const algorithmOptions = [
   { label: "Insertion", value: "insertion" },
   { label: "Selection", value: "selection" },
 ];
+
+export function generateAnimationArray(
+  selectedAlgorithm: SortingAlgorithmType,
+  isSorting: boolean,
+  array: number[],
+  runAnimation: (animations: [number[], boolean][]) => void
+) {
+  switch (selectedAlgorithm) {
+    case "bubble":
+      generateBubbleSortAnimationArray(isSorting, array, runAnimation);
+      break;
+    case "quick":
+      generateQuickSortAnimationArray(isSorting, array, runAnimation);
+      break;
+    case "merge":
+      generateMergeSortAnimationArray(isSorting, array, runAnimation);
+      break;
+    case "insertion":
+      generateInsertionSortAnimationArray(isSorting, array, runAnimation);
+      break;
+    case "selection":
+      generateSelectionSortAnimationArray(isSorting, array, runAnimation);
+      break;
+    default:
+      break;
+  }
+}
 
 export const sortingAlgorithmsData = {
   bubble: {
